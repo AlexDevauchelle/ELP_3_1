ec0a96edc9c3289d5746533268e1ebcdd0d9dc20
NicolasBcg <82137844+NicolasBcg@users.noreply.github.com>
2021-10-10 11:25:20 +0200

    Update main.go
    j'ai changé le type des temps des events de float32 à int (pour pouvoir les gérer dans le heap) et j'ai ajouté la priorityQueue adaptée à la structure event (avec une fonction test pour l'exemple)(tout marche normalement)

--- main.go
+++ main.go
@@ -3,30 +3,80 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"container/heap"
 )
 
 type Arrive struct {
 	origine         [2]int
 	localisation    [2]int
-	temps_trajet    float32
+	temps_trajet    int
 	is_Intervention bool
 }
 
 type Depart struct {
 	origine         [2]int
 	destination     [2]int
-	temps_trajet    float32
+	temps_trajet    int
 	is_Intervention bool
 }
 
 type Event struct {
-	temp_event      float32
+	temp_event      int
 	origine         [2]int
 	localisation    [2]int
 	agents_required int // le nombre d'agents demandés pour l'intervention
 }
 
-func newEvent(temp_event float32, origine [2]int, localisation [2]int, agents_required int) {
+type PriorityQueue []*Event //crée une liste d'éléments Event
+func (pq PriorityQueue) Len() int { return len(pq) }//donne la taille de la file
+
+func (pq PriorityQueue) Less(i, j int) bool {//compare les prioritées
+	return pq[i].temp_event < pq[j].temp_event
+}
+func (pq PriorityQueue) Swap(i, j int) {//échange les éléments de rang i et j 
+	pq[i], pq[j] = pq[j], pq[i]
+}
+func (pq *PriorityQueue) Push(x interface{}) {//ajoute un élément à sa place dans la liste
+	event := x.(*Event)
+	*pq = append(*pq, event)
+}
+func (pq *PriorityQueue) Pop() interface{} {//donne l'élément prioritaire de la liste et le fait sortir de celle ci
+	old := *pq
+	n := len(old)
+	event := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	*pq = old[0 : n-1]
+	return event
+}
+func (pq *PriorityQueue) update(event *Event, temp_event int) {//change le temps de trajet (possibilité de changer d'autre trucs)
+	event.temp_event = temp_event
+	heap.Fix(pq, event.temp_event)
+}
+func Example_priorityQueue() {
+	var event1 = Event{int(100),[2]int{1, 2}, [2]int{3, 4}, int(2)}
+	var event2 = Event{ int(20),[2]int{3, 4}, [2]int{1, 2}, int(3)}
+	var event3 = Event{int(50), [2]int{3, 4}, [2]int{1, 2}, int(2)}
+
+	events := [3]Event{event1,event2,event3}
+	// Create a priority queue, put the events in it, and
+	pq := make(PriorityQueue, len(events))
+	for i := 0;i<pq.Len();i++  {
+		pq[i] = &events[i]	
+	}
+	heap.Init(&pq)
+	// Insert a new event and then modify its priority.	
+	var event4 = Event{int(60),[2]int{3, 4}, [2]int{1, 2}, int(1)}
+	heap.Push(&pq, &event4)
+	// Take the items out; they arrive in increasing priority order.
+	for pq.Len() > 0 {
+		event := heap.Pop(&pq).(*Event)
+		fmt.Println("--------------------------------------------------")
+		fmt.Printf("l'évènement a lieu au temps %d et requiert %d agents",event.temp_event,event.agents_required)
+		fmt.Println(".")
+	}
+}
+
+func newEvent(temp_event int, origine [2]int, localisation [2]int, agents_required int) {
 	proba := rand.Float64()
 	lim_proba := 0.5 // est la proba d'avoir une intervention qui pop, à determiner en fonction de la fréquence des inter voulues
 
@@ -76,14 +126,16 @@ func affichageDepart(d Depart) { // déclaration de ma méthode Affichage() lié
 
 func main() {
 	fmt.Println("Hello world")
-	maArrive := Arrive{[2]int{1, 2}, [2]int{3, 4}, float32(3.3), true}
-	maDepart := Depart{[2]int{1, 2}, [2]int{3, 4}, float32(3.3), true}
+	maArrive := Arrive{[2]int{1, 2}, [2]int{3, 4}, int(3), true}
+	maDepart := Depart{[2]int{1, 2}, [2]int{3, 4}, int(4), true}
 
 	affichageArrive(maArrive)
 	affichageDepart(maDepart)
-
+/*
 	for {
 		newEvent(5.0, [2]int{1, 2}, [2]int{1, 2}, 5)
 	}
+*/
+	Example_priorityQueue()
 
 }
